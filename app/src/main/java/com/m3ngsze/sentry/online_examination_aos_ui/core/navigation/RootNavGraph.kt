2a798fabcf96c7464f49438aac5adc235c1a121c
M3NGSZE <mengse.thun13@gmail.com>
2026-02-13 21:55:14 +0700

    profile screen header and profile info

--- app/src/main/java/com/m3ngsze/sentry/online_examination_aos_ui/core/navigation/RootNavGraph.kt
+++ app/src/main/java/com/m3ngsze/sentry/online_examination_aos_ui/core/navigation/RootNavGraph.kt
@@ -9,7 +9,7 @@ import com.m3ngsze.sentry.online_examination_aos_ui.feature.room.RoomScreen
 import com.m3ngsze.sentry.online_examination_aos_ui.feature.splash.SplashScreen
 
 fun NavGraphBuilder.rootNavGraph(navController: NavHostController) {
-    navigation(startDestination = Screen.Splash.route, route = ROOT_GRAPH_ROUT){
+    navigation(startDestination = Screen.Room.route, route = ROOT_GRAPH_ROUT){
 
         composable (route = Screen.Splash.route) { SplashScreen(navController = navController) }
 
--- app/src/main/java/com/m3ngsze/sentry/online_examination_aos_ui/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/m3ngsze/sentry/online_examination_aos_ui/feature/profile/ProfileScreen.kt
@@ -1,27 +1,153 @@
 package com.m3ngsze.sentry.online_examination_aos_ui.feature.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import com.m3ngsze.sentry.online_examination_aos_ui.R
+import com.m3ngsze.sentry.online_examination_aos_ui.core.navigation.Screen
 
 @Composable
 fun ProfileScreen(
     navController: NavHostController,
     viewModel: ProfileViewModel  = hiltViewModel()
 ){
+    LaunchedEffect (Unit) {
+        viewModel.getUserProfile()
+    }
+
     Column (
         modifier = Modifier
             .padding(start = 25.dp, bottom = 25.dp, end = 25.dp)
             .background(Color(0xFFFCFCFC))
             .fillMaxWidth()
     ){
+        ProfileHeader(
+            navController = navController
+        )
 
+        ProfileInfo(
+            navController = navController,
+            viewModel = viewModel
+        )
+    }
+}
+
+@Composable
+fun ProfileHeader(
+    navController: NavHostController,
+){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(
+            imageVector = Icons.Default.ArrowBackIosNew,
+            contentDescription = "Back to exam screen",
+            modifier = Modifier
+//                .clickable{
+//                    navController.navigate(Screen.Room.route)
+//                }
+        )
+
+        Text(
+            text = "My Profile",
+            fontWeight = FontWeight.Bold,
+            fontSize = 19.sp
+        )
+
+        Icon(
+            imageVector = Icons.Default.Edit,
+            contentDescription = "Edit personal info"
+        )
+    }
+
+    Spacer(
+        modifier = Modifier
+            .height(30.dp)
+    )
+}
+
+@Composable
+fun ProfileInfo(
+    navController: NavHostController,
+    viewModel: ProfileViewModel  = hiltViewModel()
+){
+    val user = viewModel.userState
+    Row (
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Gray, CircleShape)
+        ) {
+            AsyncImage(
+                model = user?.profileUrl
+                    ?: R.drawable.profile,
+                contentDescription = "profile img",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(CircleShape)
+            )
+        }
+
+
+        Column (
+            modifier = Modifier
+                .padding(horizontal = 30.dp)
+        ){
+            Text(
+                text = """
+                    ${user?.firstName ?: "First Name" }
+                    ${user?.lastName ?: "Last Name" }
+                """.trimIndent(),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 23.sp,
+                lineHeight = 30.sp
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(5.dp)
+            )
+
+            Text(
+                text = user?.email ?: "example@gmail.com",
+                fontWeight = FontWeight.Medium,
+                fontSize = 15.sp
+            )
+        }
     }
 }
