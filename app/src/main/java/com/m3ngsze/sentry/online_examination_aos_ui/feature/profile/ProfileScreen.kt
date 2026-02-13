3c1e8062a8c068276a8c348e9cafee1d78133da5
M3NGSZE <mengse.thun13@gmail.com>
2026-02-13 22:23:19 +0700

    change layout profile info ui

--- app/src/main/java/com/m3ngsze/sentry/online_examination_aos_ui/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/m3ngsze/sentry/online_examination_aos_ui/feature/profile/ProfileScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -39,9 +40,9 @@ fun ProfileScreen(
     navController: NavHostController,
     viewModel: ProfileViewModel  = hiltViewModel()
 ){
-    LaunchedEffect (Unit) {
-        viewModel.getUserProfile()
-    }
+//    LaunchedEffect (Unit) {
+//        viewModel.getUserProfile()
+//    }
 
     Column (
         modifier = Modifier
@@ -110,7 +111,7 @@ fun ProfileInfo(
     ) {
         Box(
             modifier = Modifier
-                .size(100.dp)
+                .size(120.dp)
                 .background(Color.Gray, CircleShape)
         ) {
             AsyncImage(
@@ -123,15 +124,16 @@ fun ProfileInfo(
             )
         }
 
-
-        Column (
+        Spacer(
             modifier = Modifier
-                .padding(horizontal = 30.dp)
-        ){
+                .width(20.dp)
+        )
+
+        Column {
             Text(
                 text = """
-                    ${user?.firstName ?: "First Name" }
                     ${user?.lastName ?: "Last Name" }
+                    ${user?.firstName ?: "First Name" }
                 """.trimIndent(),
                 fontWeight = FontWeight.SemiBold,
                 fontSize = 23.sp,
@@ -140,14 +142,79 @@ fun ProfileInfo(
 
             Spacer(
                 modifier = Modifier
-                    .height(5.dp)
+                    .height(10.dp)
             )
 
-            Text(
-                text = user?.email ?: "example@gmail.com",
-                fontWeight = FontWeight.Medium,
-                fontSize = 15.sp
-            )
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Column {
+                    Text(
+                        text = "N/A",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp
+                    )
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(5.dp)
+                    )
+
+                    Text(
+                        text = "GENDER",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 14.sp,
+                        color = Color(0xFF959595)
+                    )
+                }
+
+                Column {
+                    Text(
+                        text = "67",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp
+                    )
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(5.dp)
+                    )
+
+                    Text(
+                        text = "AGE",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 14.sp,
+                        color = Color(0xFF959595)
+                    )
+                }
+
+                Column {
+                    Text(
+                        text = "67/67/6767",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp
+                    )
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(5.dp)
+                    )
+
+                    Text(
+                        text = "BIRTHDAY",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 14.sp,
+                        color = Color(0xFF959595)
+                    )
+                }
+            }
         }
     }
+
+    Spacer(
+        modifier = Modifier
+            .height(30.dp)
+    )
 }
